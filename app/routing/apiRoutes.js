c45c1dbe8d4df28f8cefff7e4b9ffcbee6cc8939
Kmasteller <kennethmasteller@gmail.com>
2018-01-01 18:24:35 -0700

    cleaned - not pushing to html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,11 +4,11 @@ var friends = require("../data/friends.js");
 
 module.exports = function(app) {
 
-    app.get("/api/friends", function (req, res, next) {
+    app.get("/api/friends", function (req, res) {
        res.json(friends);
     });
 
-    app.post("/api/friends", function(req, res, next) {
+    app.post("/api/friends", function(req, res) {
         // res.json(friends);
         // if(req.body !== {}){;
         // res.json(200)
@@ -23,9 +23,9 @@ module.exports = function(app) {
         console.log(req.body);
 
         var userData = req.body;
-        var userScore = userData.scores;
+        var userScores = userData.scores;
 
-        console.log (userScore);
+        console.log (userScores);
 
         var totalDifference = 0;
 
@@ -33,7 +33,7 @@ module.exports = function(app) {
             console.log(friends[i]);
             totalDifference = 0;
             for (var j=0; j<friends[i].scores[i]; j++) {    
-                totalDifference += Math.abs(parseInt(userScore[j]) - parseInt(friends[i].scores[j]));
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
                 if (totalDifference <= match.difference) {
                     match.name = friends[i].name;
                     match.photo = friends[i].photo;
